8c3ec192d4be56c6d3ed468ab300aab52510e135
VDB <namlun96@gmail.com>
2020-12-08 19:43:13 +0700

    .

--- DeThi/src/Test/DAO.java
+++ DeThi/src/Test/DAO.java
@@ -41,4 +41,105 @@ public class DAO {
 		}
 		return list;
 	}
+
+	public boolean addStudent(Student s) {
+		String sql = "INSERT INTO STUDENT VALUES(?,?,?,?,?,?,?,?,?)";
+		try {
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1, s.getMaSV());
+			ps.setString(2, s.getHoTen());
+			ps.setString(3, s.getIdClass());
+			ps.setDate(4, (Date) s.getNgaySinh());
+			ps.setString(5, s.getGioiTinh());
+			ps.setString(6, s.getDiaChi());
+			ps.setString(7, s.getDienThoai());
+			ps.setString(8, s.getKhoaNganh());
+			ps.setString(9, s.getLoginSV());
+			return ps.executeUpdate() > 0;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	public boolean deleteStudent(String maSV) {
+		String sql = "DELETE STUDENT WHERE MASV = ?";
+		try {
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1, maSV);
+			return ps.executeUpdate() > 0;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	public boolean updateStudent(String maSV, Student s) {
+		String sql = "UPDATE TABLE STUDENT SET HOTEN = ?, IDCLASS = ?, NGAYSINH = ?, GIOITINH = ?, DIACHI = ?, DIENTHOAI = ?, KHOANGANH = ?, LOGINSV = ? WHERE MASV = ?";
+		try {
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1, s.getHoTen());
+			ps.setString(2, s.getIdClass());
+			ps.setDate(3, (Date) s.getNgaySinh());
+			ps.setString(4, s.getGioiTinh());
+			ps.setString(5, s.getDiaChi());
+			ps.setString(6, s.getDienThoai());
+			ps.setString(7, s.getKhoaNganh());
+			ps.setString(8, s.getLoginSV());
+			ps.setString(9, maSV);
+			return ps.executeUpdate() > 0;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	public ArrayList<String> getListIDClass() {
+		ArrayList<String> list = new ArrayList<String>();
+		String sql = "SELECT*FROM CLASS";
+		try {
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				list.add(rs.getString(1));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	public ArrayList<String> getListNameClass() {
+		ArrayList<String> list = new ArrayList<String>();
+		String sql = "SELECT*FROM CLASS";
+		try {
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				list.add(rs.getString(2));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	public ArrayList<Users> getListUsers() {
+		ArrayList<Users> list = new ArrayList<>();
+		String sql = "SELECT*FROM USERS";
+		try {
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				Users u = new Users();
+				u.setUsername(rs.getString(1));
+				u.setPassword(rs.getString(2));
+				u.setChucnang(rs.getString(3));
+				list.add(u);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
 }
---
+++ DeThi/src/Test/Users.java
@@ -0,0 +1,37 @@
+package Test;
+
+public class Users {
+	private String username;
+	private String password;
+	private String chucnang;
+	
+	public Users() {
+		super();
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getChucnang() {
+		return chucnang;
+	}
+
+	public void setChucnang(String chucnang) {
+		this.chucnang = chucnang;
+	}
+	
+	
+}
